0be16b25b4795b1c066810a5052896fdb9d89a8b
KoftaBozo2235 <omarehab2217@gmail.com>
2023-04-24 15:35:52 -0700

    Automated Pusher

--- _printf.c
+++ _printf.c
@@ -1,66 +1,66 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void print_buffer(char hamdy[], int *hamdy1);
 
 /**
  * _printf - Printf function
- * @format: format.
+ * @hamdy2: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int _printf(const char *hamdy2, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    int hamdy3, hamdy4 = 0, hamdy5 = 0;
+    int hamdy6, hamdy7, hamdy8, hamdy9, hamdy1 = 0;
+    va_list hamdy10;
+    char hamdy[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
+    if (hamdy2 == NULL)
+        return (-1);
 
-	va_start(list, format);
+    va_start(hamdy10, hamdy2);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+    for (hamdy3 = 0; hamdy2 && hamdy2[hamdy3] != '\0'; hamdy3++)
+    {
+        if (hamdy2[hamdy3] != '%')
+        {
+            hamdy[hamdy1++] = hamdy2[hamdy3];
+            if (hamdy1 == BUFF_SIZE)
+                print_buffer(hamdy, &hamdy1);
+            /* write(1, &hamdy2[hamdy3], 1);*/
+            hamdy5++;
+        }
+        else
+        {
+            print_buffer(hamdy, &hamdy1);
+            hamdy6 = get_flags(hamdy2, &hamdy3);
+            hamdy7 = get_width(hamdy2, &hamdy3, hamdy10);
+            hamdy8 = get_precision(hamdy2, &hamdy3, hamdy10);
+            hamdy9 = get_size(hamdy2, &hamdy3);
+            ++hamdy3;
+            hamdy4 = handle_print(hamdy2, &hamdy3, hamdy10, hamdy,
+                hamdy6, hamdy7, hamdy8, hamdy9);
+            if (hamdy4 == -1)
+                return (-1);
+            hamdy5 += hamdy4;
+        }
+    }
 
-	print_buffer(buffer, &buff_ind);
+    print_buffer(hamdy, &hamdy1);
 
-	va_end(list);
+    va_end(hamdy10);
 
-	return (printed_chars);
+    return (hamdy5);
 }
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @hamdy: Array of chars
+ * @hamdy1: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_buffer(char hamdy[], int *hamdy1)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+    if (*hamdy1 > 0)
+        write(1, &hamdy[0], *hamdy1);
 
-	*buff_ind = 0;
+    *hamdy1 = 0;
 }
